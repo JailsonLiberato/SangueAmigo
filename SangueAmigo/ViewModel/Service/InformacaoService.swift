ba4535c324011a6bcb7022b353ce2bdd19b330f4
Jailson Liberato <jailsonliberato@gmail.com>
2018-05-15 22:35:48 -0300

    Correções nas dúvidas.

--- SangueAmigo/ViewModel/Service/InformacaoService.swift
+++ SangueAmigo/ViewModel/Service/InformacaoService.swift
@@ -18,7 +18,8 @@ class InformacaoService{
     
      func list(_ tipo:String,_ callback: @escaping (_ informacoes:Array<Informacao>?, _ error:Error?) -> Void) {
         var informacoes : Array<Informacao> = []
-        let _ = ref.child("informacoes").child(tipo.lowercased()).observeSingleEvent(of: .value ,with: { (snapshot) in
+        let tipoFormatado = tipo.lowercased().replacingOccurrences(of: "ã", with: "a").replacingOccurrences(of:"ó", with: "o").replacingOccurrences(of:"ç", with: "c").replacingOccurrences(of:"õ", with: "o")
+        let _ = ref.child("informacoes").child(tipoFormatado).observeSingleEvent(of: .value ,with: { (snapshot) in
             let lista = snapshot.value as? [String: Any]
            for i in lista! {
                 let dict =  i.value as! NSDictionary
